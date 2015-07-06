6125a61318a62d998e0cd4d956511cabe7f55af7
joest <chaojiong.zheng@gmail.com>
2015-07-07 02:09:01 +0800

    Check wheather exists origin repo

--- install.sh
+++ install.sh
@@ -2,8 +2,10 @@
 
 # backup
 echo "Backing up old dotfiles configs"
-today=`date +%Y%m%d`
-mv $HOME/.dotfiles $HOME/dotfiles.$today
+if [ -e $HOME/.dotfiles ]; then
+    today=`date +%Y%m%d`
+    mv $HOME/.dotfiles $HOME/.dotfiles.$today
+fi
 
 echo "Clone new dot files ..."
 /usr/bin/env git clone git@github.com:joest67/dotfiles.git ~/.dotfiles || exit 1
