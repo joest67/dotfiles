05be78f62c349d79b32438633750c8a8452026f7
joest <chaojiong.zheng@gmail.com>
2018-03-13 13:56:43 +0800

    wrapper configuration

--- rc_update.sh
+++ rc_update.sh
@@ -6,20 +6,20 @@
 ########################################
 rc_files=(
     zshrc
-    pip
     gitconfig
-    byobu
     tmux.conf
 )
 
+today=`date +%Y%m%d%H%M%S`
 for source_file in "${rc_files[@]}"
 do
-    echo "replace $source_file"
+    echo "==== replace $source_file ===="
     origin_file="$HOME/.$source_file"
+    echo $origin_file
     if [ -f "$origin_file" ]
     then
-        echo "origin file $origin_file exists, make backup"
+        echo "origin file $origin_file exists, make backup => $origin_file.bak_$today"
         mv $origin_file{,.bak}
     fi
-    ln -sf $HOME/.dotfiles/$source_file $origin_file
+    ln -sf $HOME/.dotfiles/conf/$source_file $origin_file
 done
