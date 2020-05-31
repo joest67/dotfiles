5bc6b2fbdf0b7ce9129d6695efe96c6cb45cd97a
joest <chaojiong.zheng@gmail.com>
2020-05-31 16:39:54 +0800

    changelist
    - use antigen manage zsh plugin
    - new tigrc for quick key binding

--- rc_update.sh
+++ rc_update.sh
@@ -9,18 +9,20 @@ rc_files=(
     gitconfig
     gitignore_global
     tmux.conf
+    tigrc
 )
 
 today=`date +%Y%m%d%H%M%S`
-for source_file in "${rc_files[@]}"
+for rc_file in "${rc_files[@]}"
 do
-    echo "==== replace $source_file ===="
-    origin_file="$HOME/.$source_file"
+    echo "replace $rc_file..."
+    link_file="$HOME/.$rc_file"
+    dest_file="$HOME/.dotfiles/conf/rc/$rc_file"
     echo $origin_file
-    if [ -f "$origin_file" ]
+    if [ -f "$link_file" ]
     then
-        echo "origin file $origin_file exists, make backup => $origin_file.bak_$today"
-        mv $origin_file{,.bak}
+        echo "$link_file exists, make backup => $link_file.bak_$today"
+        mv $link_file{,.bak}
     fi
-    ln -sf $HOME/.dotfiles/conf/$source_file $origin_file
+    ln -sf $dest_file $link_file
 done
